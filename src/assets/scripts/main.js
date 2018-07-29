61392db286a9d49201e58585853104d2849493f9
Dmitriy <dmitriy.troy@gmail.com>
2018-07-29 22:29:08 +0300

    almost done

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -21,13 +21,30 @@
     oppMenu.addEventListener("click", toggleClassMenu, false);
     myMenu.addEventListener("click", toggleClassMenu, false);
 
+    const progress = new Siema({
+      selector: '.updates-slider',
+      perPage: {
+        320: 2,
+        600: 3
+      },
+      onChange: function() {
+        const target = document.querySelector('.updates-slider');
+        if (progress.currentSlide === 1) {
+          target.style.paddingLeft = "12%";
+        } else {
+          target.style.paddingLeft = "20px";
+        }
+
+      }
+    })
+
     const siema = new Siema({
       perPage: {
         1024: 3,
-      },
+      }
     });
 
-    const siemaControls = document.querySelectorAll('.siema-controls button');
+    const siemaControls = document.querySelectorAll('.updates-container .siema-controls button');
     siemaControls[0].classList.add('active');
     const buttons = Array.prototype.slice.call(siemaControls);
     for (let i of buttons) {
@@ -39,6 +56,17 @@
       });
     }
 
+    const updatesControls = document.querySelectorAll('.slider-container .siema-controls button')
+    updatesControls[0].classList.add('active');
+    const controlButtons = Array.prototype.slice.call(updatesControls);
+    for (let i of controlButtons) {
+      i.addEventListener('click', () => {
+        progress.goTo(controlButtons.indexOf(i))
+        let active = document.querySelector('.active');
+        active.classList.remove('active');
+        updatesControls[controlButtons.indexOf(i)].classList.add('active');
+      });
+    }
   });
 
 })(jQuery);
